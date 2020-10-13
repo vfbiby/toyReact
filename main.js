540750ba220f9b637439b5d7c41799ca62698e4f
James Zhang <liulfbaby@gmail.com>
2020-10-13 11:53:58 +0800

    toyReact can transform a simple div without attributes

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ for(let i of [1, 2, 3]){
   console.log(i);
 }
 
-let a = <div/>
+function createElement(tagName, attributes, ...children){
+  return document.createElement(tagName)
+}
+
+window.a = <div id="a" class="c">
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
