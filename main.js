7ea1c49cbeda3ec964f69695393fd3bf143cdc5a
James Zhang <liulfbaby@gmail.com>
2020-10-13 15:56:04 +0800

    Split to a single file toy-react.js

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-for(let i of [1, 2, 3]){
-  console.log(i);
+import { createElement } from "./toy-react";
+
+class MyComponent{
+
 }
 
-function createElement(tagName, attributes, ...children){
-  let e = document.createElement(tagName)
-  for(let p in attributes){
-    e.setAttribute(p, attributes[p])
-  }
-
-  for(let child of children){
-    if(typeof child === "string"){
-      child = document.createTextNode(child)
-    }
-    e.appendChild(child)
-  }
-
-  return e;
-}
-
-document.body.appendChild(<div id="a" class="c">
+document.body.appendChild(<MyComponent id="a" class="c">
   <div>abc</div>
   <div></div>
   <div></div>
-</div>)
+</MyComponent>)
---
+++ toy-react.js
@@ -0,0 +1,22 @@
+
+export function createElement(type, attributes, ...children){
+  let e;
+  if(typeof type === 'string'){
+    e = document.createElement(type)
+  }else{
+    e = new type
+  }
+
+  for(let p in attributes){
+    e.setAttribute(p, attributes[p])
+  }
+
+  for(let child of children){
+    if(typeof child === "string"){
+      child = document.createTextNode(child)
+    }
+    e.appendChild(child)
+  }
+
+  return e;
+}
