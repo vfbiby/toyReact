818fb309487bfb4830c5db212dd0c26a15b25c8e
James Zhang <liulfbaby@gmail.com>
2020-10-13 17:04:04 +0800

    ToyReact can compile custom component with three class, ElementWrapper, TextWrapper, Component and 1 function render

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-import { createElement } from "./toy-react";
+import { createElement, Component, render } from "./toy-react";
 
-class MyComponent{
-
+class MyComponent extends Component{
+  render(){
+    return <div>
+      <h1>my component</h1>
+      {this.children}
+    </div>
+  }
 }
 
-document.body.appendChild(<MyComponent id="a" class="c">
+render(<MyComponent id="a" class="c">
   <div>abc</div>
   <div></div>
   <div></div>
-</MyComponent>)
+</MyComponent>, document.body)
--- toy-react.js
+++ toy-react.js
@@ -1,8 +1,47 @@
 
+class ElementWrapper{
+  constructor(type){
+    this.root = document.createElement(type)
+  }
+  setAttribute(name, value){
+    this.root.setAttribute(name, value)
+  }
+  appendChild(component){
+    this.root.appendChild(component.root)
+  }
+}
+
+class TextWrapper{
+  constructor(content){
+    this.root = document.createTextNode(content)
+  }
+}
+
+export class Component {
+  constructor(){
+    this.props = Object.create(null)
+    this.children = []
+    this._root = null
+  }
+  setAttribute(name, value){
+    this.props[name] = value
+  }
+  appendChild(component){
+    this.children.push(component)
+  }
+
+  get root(){
+    if(!this._root){
+      this._root = this.render().root
+    }
+    return this._root
+  }
+}
+
 export function createElement(type, attributes, ...children){
   let e;
   if(typeof type === 'string'){
-    e = document.createElement(type)
+    e = new ElementWrapper(type)
   }else{
     e = new type
   }
@@ -11,12 +50,23 @@ export function createElement(type, attributes, ...children){
     e.setAttribute(p, attributes[p])
   }
 
-  for(let child of children){
-    if(typeof child === "string"){
-      child = document.createTextNode(child)
+  let insertChildren = (children) => {
+    for(let child of children){
+      if(typeof child === "string"){
+        child = new TextWrapper(child)
+      }
+      if((typeof child === 'object') && (child instanceof Array)){
+        insertChildren(child)
+      }else{
+        e.appendChild(child)
+      }
     }
-    e.appendChild(child)
   }
+  insertChildren(children)
 
   return e;
+}
+
+export function render(component, parentElement){
+  parentElement.appendChild(component.root)
 }
